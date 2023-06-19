5be4150c4e413e1744aaa811ee9fd1ae95ed59a7
eva <evaandrianavalona@gmail.com>
2023-06-19 10:48:26 +0300

    amelioration du page bathome

---
+++ assets/js/validator.js
@@ -0,0 +1,15 @@
+window.onload = ()=>{
+    let myform = document.getElementById('myform');
+    myform.addEventListener('submit' ,function(e){
+        let email = document.getElementById('email');
+        if(email.value == ''){
+            let invalid = document.getElementById('invalid');
+            invalid.innerHTML = 'Le Champ ne doit pas etre vide';
+            invalid.style.color = 'red';
+        }
+        else {
+            e.preventDefault();
+            document.getElementById('popupbox').style.display = 'block';
+        }
+    });
+}
